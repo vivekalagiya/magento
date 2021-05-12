2db7c9e408ea38ad5b3c9ccd72b2951b0eae2b84
Vivek Alagiya <vivekalagiya111@gmail.com>
2021-05-12 17:39:06 +0530

    update script

--- app/code/local/Ccc/Browse/sql/browse_setup/mysql4-upgrade-0.1.0-0.2.0.php
+++ app/code/local/Ccc/Browse/sql/browse_setup/mysql4-upgrade-0.1.0-0.2.0.php
@@ -1,114 +1,114 @@
 <?php
 
-$installer = $this;
+// $installer = $this;
 
 
-$table = $installer->getConnection()
-    ->newTable($installer->getTable('browse/eav_attribute'))
-    ->addColumn('attribute_id', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, array(
-        'unsigned' => true,
-        'nullable' => false,
-        'primary'  => true,
-    ), 'Attribute ID')
-    ->addColumn('frontend_input_renderer', Varien_Db_Ddl_Table::TYPE_TEXT, 255, array(
-    ), 'Frontend Input Renderer')
-    ->addColumn('is_global', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, array(
-        'unsigned' => true,
-        'nullable' => false,
-        'default'  => '1',
-    ), 'Is Global')
-    ->addColumn('is_visible', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, array(
-        'unsigned' => true,
-        'nullable' => false,
-        'default'  => '1',
-    ), 'Is Visible')
-    ->addColumn('is_searchable', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, array(
-        'unsigned' => true,
-        'nullable' => false,
-        'default'  => '0',
-    ), 'Is Searchable')
-    ->addColumn('is_filterable', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, array(
-        'unsigned' => true,
-        'nullable' => false,
-        'default'  => '0',
-    ), 'Is Filterable')
-    ->addColumn('is_comparable', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, array(
-        'unsigned' => true,
-        'nullable' => false,
-        'default'  => '0',
-    ), 'Is Comparable')
-    ->addColumn('is_visible_on_front', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, array(
-        'unsigned' => true,
-        'nullable' => false,
-        'default'  => '0',
-    ), 'Is Visible On Front')
-    ->addColumn('is_html_allowed_on_front', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, array(
-        'unsigned' => true,
-        'nullable' => false,
-        'default'  => '0',
-    ), 'Is HTML Allowed On Front')
-    ->addColumn('is_used_for_price_rules', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, array(
-        'unsigned' => true,
-        'nullable' => false,
-        'default'  => '0',
-    ), 'Is Used For Price Rules')
-    ->addColumn('is_filterable_in_search', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, array(
-        'unsigned' => true,
-        'nullable' => false,
-        'default'  => '0',
-    ), 'Is Filterable In Search')
-    ->addColumn('used_in_product_listing', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, array(
-        'unsigned' => true,
-        'nullable' => false,
-        'default'  => '0',
-    ), 'Is Used In Product Listing')
-    ->addColumn('used_for_sort_by', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, array(
-        'unsigned' => true,
-        'nullable' => false,
-        'default'  => '0',
-    ), 'Is Used For Sorting')
-    ->addColumn('is_configurable', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, array(
-        'unsigned' => true,
-        'nullable' => false,
-        'default'  => '1',
-    ), 'Is Configurable')
-    ->addColumn('apply_to', Varien_Db_Ddl_Table::TYPE_TEXT, 255, array(
-        'nullable' => true,
-    ), 'Apply To')
-    ->addColumn('is_visible_in_advanced_search', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, array(
-        'unsigned' => true,
-        'nullable' => false,
-        'default'  => '0',
-    ), 'Is Visible In Advanced Search')
-    ->addColumn('position', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
-        'nullable' => false,
-        'default'  => '0',
-    ), 'Position')
-    ->addColumn('is_wysiwyg_enabled', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, array(
-        'unsigned' => true,
-        'nullable' => false,
-        'default'  => '0',
-    ), 'Is WYSIWYG Enabled')
-    ->addColumn('is_used_for_promo_rules', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, array(
-        'unsigned' => true,
-        'nullable' => false,
-        'default'  => '0',
-    ), 'Is Used For Promo Rules')
-    ->addIndex($installer->getIdxName('browse/eav_attribute', array('used_for_sort_by')),
-        array('used_for_sort_by'))
-    ->addIndex($installer->getIdxName('browse/eav_attribute', array('used_in_product_listing')),
-        array('used_in_product_listing'))
-    ->addForeignKey($installer->getFkName('browse/eav_attribute', 'attribute_id', 'eav/attribute', 'attribute_id'),
-        'attribute_id', $installer->getTable('eav/attribute'), 'attribute_id',
-        Varien_Db_Ddl_Table::ACTION_CASCADE, Varien_Db_Ddl_Table::ACTION_CASCADE)
-    ->setComment('browse EAV Attribute Table');
+// $table = $installer->getConnection()
+//     ->newTable($installer->getTable('browse/eav_attribute'))
+//     ->addColumn('attribute_id', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, array(
+//         'unsigned' => true,
+//         'nullable' => false,
+//         'primary'  => true,
+//     ), 'Attribute ID')
+//     ->addColumn('frontend_input_renderer', Varien_Db_Ddl_Table::TYPE_TEXT, 255, array(
+//     ), 'Frontend Input Renderer')
+//     ->addColumn('is_global', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, array(
+//         'unsigned' => true,
+//         'nullable' => false,
+//         'default'  => '1',
+//     ), 'Is Global')
+//     ->addColumn('is_visible', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, array(
+//         'unsigned' => true,
+//         'nullable' => false,
+//         'default'  => '1',
+//     ), 'Is Visible')
+//     ->addColumn('is_searchable', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, array(
+//         'unsigned' => true,
+//         'nullable' => false,
+//         'default'  => '0',
+//     ), 'Is Searchable')
+//     ->addColumn('is_filterable', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, array(
+//         'unsigned' => true,
+//         'nullable' => false,
+//         'default'  => '0',
+//     ), 'Is Filterable')
+//     ->addColumn('is_comparable', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, array(
+//         'unsigned' => true,
+//         'nullable' => false,
+//         'default'  => '0',
+//     ), 'Is Comparable')
+//     ->addColumn('is_visible_on_front', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, array(
+//         'unsigned' => true,
+//         'nullable' => false,
+//         'default'  => '0',
+//     ), 'Is Visible On Front')
+//     ->addColumn('is_html_allowed_on_front', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, array(
+//         'unsigned' => true,
+//         'nullable' => false,
+//         'default'  => '0',
+//     ), 'Is HTML Allowed On Front')
+//     ->addColumn('is_used_for_price_rules', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, array(
+//         'unsigned' => true,
+//         'nullable' => false,
+//         'default'  => '0',
+//     ), 'Is Used For Price Rules')
+//     ->addColumn('is_filterable_in_search', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, array(
+//         'unsigned' => true,
+//         'nullable' => false,
+//         'default'  => '0',
+//     ), 'Is Filterable In Search')
+//     ->addColumn('used_in_product_listing', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, array(
+//         'unsigned' => true,
+//         'nullable' => false,
+//         'default'  => '0',
+//     ), 'Is Used In Product Listing')
+//     ->addColumn('used_for_sort_by', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, array(
+//         'unsigned' => true,
+//         'nullable' => false,
+//         'default'  => '0',
+//     ), 'Is Used For Sorting')
+//     ->addColumn('is_configurable', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, array(
+//         'unsigned' => true,
+//         'nullable' => false,
+//         'default'  => '1',
+//     ), 'Is Configurable')
+//     ->addColumn('apply_to', Varien_Db_Ddl_Table::TYPE_TEXT, 255, array(
+//         'nullable' => true,
+//     ), 'Apply To')
+//     ->addColumn('is_visible_in_advanced_search', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, array(
+//         'unsigned' => true,
+//         'nullable' => false,
+//         'default'  => '0',
+//     ), 'Is Visible In Advanced Search')
+//     ->addColumn('position', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+//         'nullable' => false,
+//         'default'  => '0',
+//     ), 'Position')
+//     ->addColumn('is_wysiwyg_enabled', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, array(
+//         'unsigned' => true,
+//         'nullable' => false,
+//         'default'  => '0',
+//     ), 'Is WYSIWYG Enabled')
+//     ->addColumn('is_used_for_promo_rules', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, array(
+//         'unsigned' => true,
+//         'nullable' => false,
+//         'default'  => '0',
+//     ), 'Is Used For Promo Rules')
+//     ->addIndex($installer->getIdxName('browse/eav_attribute', array('used_for_sort_by')),
+//         array('used_for_sort_by'))
+//     ->addIndex($installer->getIdxName('browse/eav_attribute', array('used_in_product_listing')),
+//         array('used_in_product_listing'))
+//     ->addForeignKey($installer->getFkName('browse/eav_attribute', 'attribute_id', 'eav/attribute', 'attribute_id'),
+//         'attribute_id', $installer->getTable('eav/attribute'), 'attribute_id',
+//         Varien_Db_Ddl_Table::ACTION_CASCADE, Varien_Db_Ddl_Table::ACTION_CASCADE)
+//     ->setComment('browse EAV Attribute Table');
 
-$query = "ALTER TABLE `browse_varchar` ADD UNIQUE( `attribute_id`, `store_id`, `entity_id`)";
-$installer->getConnection()->query($query);
+// $query = "ALTER TABLE `browse_varchar` ADD UNIQUE( `attribute_id`, `store_id`, `entity_id`)";
+// $installer->getConnection()->query($query);
 
-$installer->getConnection()->createTable($table);
+// $installer->getConnection()->createTable($table);
 
-$setup = new Mage_Eav_Model_Entity_Setup('core_setup');
+// $setup = new Mage_Eav_Model_Entity_Setup('core_setup');
 
 // $setup->addAttribute(Ccc_Browse_Model_Resource_Browse::ENTITY, 'name', array(
 //     'group'                      => 'General',
@@ -184,4 +184,4 @@ $setup = new Mage_Eav_Model_Entity_Setup('core_setup');
 //     'global'                     => Ccc_Browse_Model_Resource_Eav_Attribute::SCOPE_STORE,
 // ));
 
-$installer->endSetup();
+// $installer->endSetup();
