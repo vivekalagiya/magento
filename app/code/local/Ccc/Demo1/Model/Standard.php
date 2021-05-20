63c398f0b3b162185daf0272dcef3f53c873821a
Vivek Alagiya <vivekalagiya111@gmail.com>
2021-05-20 19:42:54 +0530

    add payment method

---
+++ app/code/local/Ccc/Demo1/Model/Standard.php
@@ -0,0 +1,23 @@
+<?php
+ 
+class Ccc_Demo1_Model_Standard extends Mage_Payment_Model_Method_Abstract
+{
+ 
+protected $_code = 'demo1';
+ 
+// protected $_isInitializeNeeded      = true;
+// protected $_canUseInternal          = false;
+// protected $_canUseForMultishipping  = false;
+ 
+// /**
+// * Return Order place redirect url
+// *
+// * @return string
+// */
+// public function getOrderPlaceRedirectUrl()
+// {
+// //when you click on place order you will be redirected on this url, if you don't want this action remove this method
+// return Mage::getUrl('customcard/standard/redirect', array('_secure' => true));
+// }
+ 
+}
